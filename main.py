576156347c2c078484778f294e02236e76387040
Jagath Kiran <jagathks2020@gmail.com>
2026-09-10 14:58:00 +1000

    Count Characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, character_count
 
 def get_book_text(path):
     with open(path) as f:
@@ -7,8 +7,9 @@ def get_book_text(path):
 def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = count_words(text)
+    character_counts = character_count(text)
+
     print(f"Found {num_words} total words")
-
-
+    print(character_counts)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,16 @@ def count_words(text):
     words = text.split()
     return (len(words))
 
+def character_count(text):
+    character_counts = {}
+
+    for character in text:
+        character = character.lower()
+
+        if character in character_counts:
+            character_counts[character] += 1
+        else:
+            character_counts[character] = 1
+
+    return character_counts
+    
