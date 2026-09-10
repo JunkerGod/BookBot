969627b5f64b71ee179ad61c48b11067fa01aae4
Jagath Kiran <jagathks2020@gmail.com>
2026-09-10 14:20:02 +1000

    Count Words

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def count_words(text):
+    words = text.split()
+    return (len(words))
 
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(text)
+    num_words = count_words(text)
+    print(f"Found {num_words} total words")
+
 
 
 main()
