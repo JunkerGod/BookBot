b5665ce2cbd0121da672d25ab107480e6b8d48e0
Jagath Kiran <jagathks2020@gmail.com>
2026-09-11 23:45:55 +1000

    Finished Project

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from stats import count_words, character_count
+import sys
+
+from stats import count_words, character_count, chars_dict_to_sorted_list
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def print_report(path, num_words, sorted_characters):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for character, count in sorted_characters:
+        if character.isalpha():
+            print(f"{character}: {count}")
+    print("============= END ===============")
+
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+    text = get_book_text(path)
     num_words = count_words(text)
     character_counts = character_count(text)
+    sorted_characters = chars_dict_to_sorted_list(character_counts)
 
-    print(f"Found {num_words} total words")
-    print(character_counts)
+    print_report(path, num_words, sorted_characters)
 
 main()
--- stats.py
+++ stats.py
@@ -14,4 +14,17 @@ def character_count(text):
             character_counts[character] = 1
 
     return character_counts
-    
+
+
+def sort_on(character_count: tuple[str, int]) -> int:
+    return character_count[1]
+
+
+def chars_dict_to_sorted_list(character_counts: dict[str, int]) -> list[tuple[str, int]]:
+    characters = []
+
+    for character in character_counts:
+        count = character_counts[character]
+        characters.append((character, count))
+
+    return sorted(characters, reverse=True, key=sort_on)
