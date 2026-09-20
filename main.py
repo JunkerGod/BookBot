32c5dcbf93d3b43907a3159ad580977213ebcd19
Jagath Kiran <jagathks2020@gmail.com>
2026-09-20 20:53:18 +1000

    added comments

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import sys
 
 from stats import count_words, character_count, chars_dict_to_sorted_list
 
-def get_book_text(path):
+def get_book_text(path): #gets the book path and reads the contents
     with open(path) as f:
         return f.read()
 
-def print_report(path, num_words, sorted_characters):
+def print_report(path, num_words, sorted_characters): #formats the report
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    for character, count in sorted_characters:
+    for character, count in sorted_characters: #removes non-alphabetical characters
         if character.isalpha():
             print(f"{character}: {count}")
     print("============= END ===============")
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python3 main.py <path_to_book>")
+        print("Usage: python3 main.py <path_ito_book>") #instructions on how to use the program
         sys.exit(1)
 
     path = sys.argv[1]
--- stats.py
+++ stats.py
@@ -1,30 +1,30 @@
 def count_words(text):
     words = text.split()
-    return (len(words))
+    return (len(words)) #counts how many words in the text
 
 def character_count(text):
     character_counts = {}
 
     for character in text:
-        character = character.lower()
+        character = character.lower() #turns every chracter into lowercase
 
         if character in character_counts:
-            character_counts[character] += 1
+            character_counts[character] += 1 #if the character is found in the dictionary increment it by 1
         else:
-            character_counts[character] = 1
+            character_counts[character] = 1 #if the character is not in the dictionary add it and = it to one count
 
     return character_counts
 
 
-def sort_on(character_count: tuple[str, int]) -> int:
+def sort_on(character_count: tuple[str, int]) -> int: #returns the count of each chracter 
     return character_count[1]
 
 
-def chars_dict_to_sorted_list(character_counts: dict[str, int]) -> list[tuple[str, int]]:
+def chars_dict_to_sorted_list(character_counts: dict[str, int]) -> list[tuple[str, int]]: #turns dictionary into tuple
     characters = []
 
     for character in character_counts:
         count = character_counts[character]
         characters.append((character, count))
 
-    return sorted(characters, reverse=True, key=sort_on)
+    return sorted(characters, reverse=True, key=sort_on) #sorts by frequency highest to lowest
